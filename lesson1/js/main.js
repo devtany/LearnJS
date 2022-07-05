4be4127806f50554f4b4dd1b6d01661d92fee9cf
devtany <tanya2000z@bk.ru>
2022-07-05 18:53:08 +0400

    Tanya Dev.
    Initial commint

---
+++ lesson1/js/main.js
@@ -0,0 +1,17 @@
+// let incr = 10,
+//     decr = 10;
+// // ++incr;
+// // --decr;
+
+// console.log(incr++);
+// console.log(decr);
+
+console.log(2+2*2 != 9);
+
+const check = false,
+    close = false;
+console.log(!check || !close);
+
+
+
+
