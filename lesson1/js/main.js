da2f6efc2cc7fc5896e52e9a5a545b9a5d439ff3
devtany <tanya2000z@bk.ru>
2022-07-06 12:59:54 +0400

    practice1

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,3 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-alert(numberOfFilms);
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?'),
+    b = +prompt('На сколько  оцените его?'),
+    c = prompt('Один из последних просмотренных фильмов?'),
+    d = +prompt('На сколько  оцените его?');
+
+    personalMovieDB.movies[a] = b;
+    personalMovieDB.movies[c] = d;
+
+    console.log(personalMovieDB);
