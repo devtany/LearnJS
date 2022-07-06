b03d911a888d65da604463e22b6cab85b476306c
devtany <tanya2000z@bk.ru>
2022-07-06 12:32:30 +0400

    fkfkfk

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,16 +1,3 @@
-// let incr = 10,
-//     decr = 10;
-// // ++incr;
-// // --decr;
 
-// console.log(incr++);
-// console.log(decr);
-
-console.log(2+2*2 != 9);
-
-const check = false,
-    close = false;
-console.log(!check || !close);
-cvbgdfgdfd
-
-
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+alert(numberOfFilms);
